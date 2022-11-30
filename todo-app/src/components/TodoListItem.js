dd1ae53e6bb9de234c7ed26af02da11b2b834755
greekr4 <greekr4@naver.com>
2022-11-30 17:29:57 +0900

    20221130

--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
     MdCheckBoxOutlineBlank,
     MdCheckBox,
@@ -6,10 +7,11 @@ import {
 import cn from 'classnames'
 import './TodoListItem.scss'
 
-const TodoListItem = ({todo, onRemove, onToggle}) => {
+const TodoListItem = ({todo, onRemove, onToggle,style}) => {
     const { id, text,checked } = todo;
 return(
-    <div className='TodoListItem'>
+    <div className='TodoListItem-virtualized' style={style}>
+        <div className='TodoListItem'>
         <div className={cn('checkbox',{checked})} onClick={()=>onToggle(id)} >
             {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank/> }
             <div className='text'>{text}</div>
@@ -17,8 +19,9 @@ return(
         <div className='remove' onClick={() => onRemove(id)}>
             <MdRemoveCircleOutline/>
         </div>
+        </div>
     </div>
 
 );
 };
-export default TodoListItem;
+export default React.memo(TodoListItem);
